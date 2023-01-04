f0446b5290cc1766b4116d4be94d655885be0c83
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-01-04 23:50:01 +0000

    Initial codes

---
+++ app.js
@@ -0,0 +1,45 @@
+// Just adding async causes we have a promise return
+async function myFunc1() {
+  return "hello";
+}
+
+myFunc1().then((res) => console.log(res));
+
+// --------------------------------------------------------
+
+// async and await with promise
+
+async function myFunc() {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Hello"), 1000);
+  });
+
+  const error = false;
+
+  if (!error) {
+    const res = await promise; // Wait until promise is resolved
+    return res;
+  } else {
+    await Promise.reject(new Error("Something went wrong"));
+  }
+}
+
+myFunc()
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+// ---------------------------------------------------------
+
+// async and await with fetch
+
+async function getUser() {
+  // await response of the fetch call
+  const response = await fetch("http://jsonplaceholder.typicode.com/users");
+
+  // only proceed once its resolved
+  const data = await response.json();
+
+  // only proceed once second promise is resolved
+  return data;
+}
+
+getUser().then((users) => console.log(users));
